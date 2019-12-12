80e486ba8dc6d6947b3beca26810d34252cccdea
QiuJa <xdw#2018>
2019-12-12 16:32:28 +0800

    springboot框架

---
+++ src/main/java/cn/com/newloading/SpringbootApplication.java
@@ -0,0 +1,26 @@
+package cn.com.newloading;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@ServletComponentScan
+@MapperScan("com.camel.dao")
+public class SpringbootApplication extends SpringBootServletInitializer {
+
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(SpringbootApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
