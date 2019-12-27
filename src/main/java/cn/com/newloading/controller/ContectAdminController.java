83fee57654ea5254dee8e8d504b3129ae708c32f
QiuJa <xdw#2018>
2019-12-27 17:11:58 +0800

    1

--- src/main/java/cn/com/newloading/controller/ContectAdminController.java
+++ src/main/java/cn/com/newloading/controller/ContectAdminController.java
@@ -31,47 +31,57 @@ public class ContectAdminController {
 	private DictService dictService;
 	@Autowired
 	private ContectAdminService caService;
-
-	@RequestMapping("/addContectAdmin")
+	
+	/**
+	 * 学生提出系统问题
+	 * @param request
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/stuSysQuestion")
 	@ResponseBody
-	public JSONObject addContectAdmin(HttpServletRequest request,@RequestBody Map<String, Object> params) {
-		String foreignType = (String) params.get("foreignType");
-		if (StringUtil.isBlank(foreignType)) {
-			return responseMsg("CONADM0002", "CONADM");
-		}
-		if (!RoleType.ADM.getRole().equals(foreignType) && !RoleType.STU.getRole().equals(foreignType)
-				&& !RoleType.TEA.getRole().equals(foreignType)) {
-			return responseMsg("CONADM0003", "CONADM");
-		}
-		
+	public JSONObject stuSysQuestion(HttpServletRequest request,@RequestBody Map<String, Object> params) {
 		ContectAdmin contectAdmin = new ContectAdmin();
-		contectAdmin.setForeignType(foreignType);
-		
-		/*取外键id*/
-		if(RoleType.STU.getRole().equals(foreignType)) {
-			Student student = (Student) request.getSession().getAttribute("student");
-			if(null == student || "".equals(student.getId())) {
-				return responseMsg("STU00006", "STUDENT");
-			}
-			contectAdmin.setForeignId(student.getId());
-		}else if(RoleType.TEA.getRole().equals(foreignType)) {
-//			Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
-//			if(null == teacher || "".equals(teacher.getId())) {
-//				return responseMsg("TEA00006", "TEACHER");
-//			}
-//			contectAdmin.setForeignId(teacher.getId());
-		}else {
-			Admin admin = (Admin) request.getSession().getAttribute("admin");
-			if(null == admin || "".equals(admin.getId())) {
-				return responseMsg("SYSMSG0004", "SYSMSG");
-			}
-			contectAdmin.setForeignId(admin.getId());
+		contectAdmin.setForeignType(RoleType.STU.getRole());
+		Student student = (Student) request.getSession().getAttribute("student");
+		if(null == student || "".equals(student.getId())) {
+			return responseMsg("STU00006", "STUDENT");
 		}
+		contectAdmin.setForeignId(student.getId());
+		//消息内容
+		String content = (String) params.get("content");
+		if(StringUtil.isBlank(content)) {
+			return responseMsg("CONADM0004", "CONADM");
+		}
+		contectAdmin.setContent(content);
 		
+		String retcode = caService.addContectAdmin(contectAdmin);
+		return responseMsg(retcode, "CONADM");
+		
+	}
+	
+	/**
+	 * 学生回复系统问题
+	 * @param request
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/stuReplySysMsg")
+	@ResponseBody
+	public JSONObject stuReplySysMsg(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.STU.getRole());
+		Student student = (Student) request.getSession().getAttribute("student");
+		if(null == student || "".equals(student.getId())) {
+			return responseMsg("STU00006", "STUDENT");
+		}
+		contectAdmin.setForeignId(student.getId());
 		//父级id，属于哪个问题下的，如果是问题说明为空
 		String pid = (String) params.get("pid");
+		if(StringUtil.isBlank(pid)) {
+			return responseMsg("CONADM0002", "CONADM");
+		}
 		contectAdmin.setPid(pid);
-		
 		//消息内容
 		String content = (String) params.get("content");
 		if(StringUtil.isBlank(content)) {
@@ -83,6 +93,101 @@ public class ContectAdminController {
 		return responseMsg(retcode, "CONADM");
 	}
 	
+	/**
+	 * 老师提出系统问题
+	 * @param request
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/teaSysQuestion")
+	@ResponseBody
+	public JSONObject teaSysQuestion(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.TEA.getRole());
+//		Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
+//		if(null == teacher || "".equals(teacher.getId())) {
+//			return responseMsg("TEA00006", "TEACHER");
+//		}
+//		contectAdmin.setForeignId(teacher.getId());
+		//消息内容
+		String content = (String) params.get("content");
+		if(StringUtil.isBlank(content)) {
+			return responseMsg("CONADM0004", "CONADM");
+		}
+		contectAdmin.setContent(content);
+		
+		String retcode = caService.addContectAdmin(contectAdmin);
+		return responseMsg(retcode, "CONADM");
+		
+	}
+
+	/**
+	 * 老师回复系统问题
+	 * @param request
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/teaReplySysMsg")
+	@ResponseBody
+	public JSONObject teaReplySysMsg(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.TEA.getRole());
+//		Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
+//		if(null == teacher || "".equals(teacher.getId())) {
+//			return responseMsg("TEA00006", "TEACHER");
+//		}
+//		contectAdmin.setForeignId(teacher.getId());
+		//父级id，属于哪个问题下的，如果是问题说明为空
+		String pid = (String) params.get("pid");
+		if(StringUtil.isBlank(pid)) {
+			return responseMsg("CONADM0002", "CONADM");
+		}
+		contectAdmin.setPid(pid);
+		//消息内容
+		String content = (String) params.get("content");
+		if(StringUtil.isBlank(content)) {
+			return responseMsg("CONADM0004", "CONADM");
+		}
+		contectAdmin.setContent(content);
+		
+		String retcode = caService.addContectAdmin(contectAdmin);
+		return responseMsg(retcode, "CONADM");
+	}
+	
+	
+	/**
+	 * 管理员回复系统问题
+	 * @param request
+	 * @param params
+	 * @return
+	 */
+	@RequestMapping("/admReplySysMsg")
+	@ResponseBody
+	public JSONObject admReplySysMsg(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.ADM.getRole());
+		Admin admin = (Admin) request.getSession().getAttribute("admin");
+		if(null == admin || "".equals(admin.getId())) {
+			return responseMsg("SYSMSG0004", "SYSMSG");
+		}
+		contectAdmin.setForeignId(admin.getId());
+		//父级id，属于哪个问题下的，如果是问题说明为空
+		String pid = (String) params.get("pid");
+		if(StringUtil.isBlank(pid)) {
+			return responseMsg("CONADM0002", "CONADM");
+		}
+		contectAdmin.setPid(pid);
+		//消息内容
+		String content = (String) params.get("content");
+		if(StringUtil.isBlank(content)) {
+			return responseMsg("CONADM0004", "CONADM");
+		}
+		contectAdmin.setContent(content);
+		
+		String retcode = caService.addContectAdmin(contectAdmin);
+		return responseMsg(retcode, "CONADM");
+	}
+
 	@RequestMapping("/queryContectAdminForAdmin")
 	@ResponseBody
 	public JSONObject queryContectAdminForAdmin(@RequestBody Map<String, Object> params) {
@@ -102,6 +207,66 @@ public class ContectAdminController {
 		if(StringUtil.isNotBlank(content)) {
 			contectAdmin.setContent(content);
 		}
+		
+		//是否有没回复
+		String replyFlag = (String) params.get("replyFlag");//可以不选择
+		if(StringUtil.isBlank(replyFlag)) {
+			contectAdmin.setReplyFlag(replyFlag);
+		}
+		
+		List<ContectAdmin> caList = caService.queryContectAdmin(contectAdmin);
+		json.put("contectAdminList", caList);
+		return json;
+	}
+	
+	@RequestMapping("/queryContectAdminForStudent")
+	@ResponseBody
+	public JSONObject queryContectAdminForStudent(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		JSONObject json = new JSONObject();
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.STU.getRole());
+		
+//		//内容提供模糊查询
+//		String content = (String) params.get("content");
+//		if(StringUtil.isNotBlank(content)) {
+//			contectAdmin.setContent(content);
+//		}
+		
+		Student student = (Student) request.getSession().getAttribute("student");
+		if(null == student || "".equals(student.getId())) {
+			return responseMsg("STU00006", "STUDENT");
+		}
+		contectAdmin.setForeignId(student.getId());
+		
+		//是否有没回复
+		String replyFlag = (String) params.get("replyFlag");//可以不选择
+		if(StringUtil.isBlank(replyFlag)) {
+			contectAdmin.setReplyFlag(replyFlag);
+		}
+		
+		List<ContectAdmin> caList = caService.queryContectAdmin(contectAdmin);
+		json.put("contectAdminList", caList);
+		return json;
+	}
+	
+	@RequestMapping("/queryContectAdminForTeacher")
+	@ResponseBody
+	public JSONObject queryContectAdminForTeacher(HttpServletRequest request,@RequestBody Map<String, Object> params) {
+		JSONObject json = new JSONObject();
+		ContectAdmin contectAdmin = new ContectAdmin();
+		contectAdmin.setForeignType(RoleType.TEA.getRole());
+		
+//		//内容提供模糊查询
+//		String content = (String) params.get("content");
+//		if(StringUtil.isNotBlank(content)) {
+//			contectAdmin.setContent(content);
+//		}
+		
+//		Teacher teacher = (Teacher) request.getSession().getAttribute("teacher");
+//		if(null == teacher || "".equals(teacher.getId())) {
+//			return responseMsg("TEA00006", "TEACHER");
+//		}
+//		contectAdmin.setForeignId(teacher.getId());
 		
 		//是否有没回复
 		String replyFlag = (String) params.get("replyFlag");//可以不选择
